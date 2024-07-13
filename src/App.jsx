1cec4e5ec044a0ca95656e5db2c17380bc139f1f
Repaer@Zet <nobeliylord@gmail.com>
2024-07-13 13:23:45 +0300

    UPD: App.jsx | change nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -256,7 +256,7 @@ function App() {
                     </div>
                     <ul className='NavList'>
                         <li id='/about' onClick={handleNavigate} onKeyDown={handleNavigate} tabIndex={0} aria-roledescription='О нас'>О нас</li>
-                        <li id='/price' onClick={handleNavigate} onKeyDown={handleNavigate} tabIndex={0} aria-roledescription='Прайс-лист'>Прайс-лист</li>
+                        <li id='/price' onClick={handleNavigate} onKeyDown={handleNavigate} tabIndex={0} aria-roledescription='Наши услуги'>Наши услуги</li>
                         <li onClick={toGallery} onKeyDown={toGallery} tabIndex={0} aria-roledescription='Фотогалерея'>Галерея</li>
                         <li id='/competition' onClick={handleNavigate} onKeyDown={handleNavigate} tabIndex={0} aria-roledescription='Конкурсы'>Конкурсы</li>
                         {/* <li id='/charity' onClick={handleNavigate}>Благотворительность</li> */}
